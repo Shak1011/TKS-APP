9c60b2b817380f85af4133d954887c2b6d374bc1
Samuel Tennant <samueltennant@hotmail.com>
2023-03-28 17:48:45 +0100

    added actix with server boilerplate

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, HttpResponse, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    actix_web::HttpServer::new(|| actix_web::App::new().service(index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("<h1>Hello world!</h1>")
 }
