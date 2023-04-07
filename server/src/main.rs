5d44d0ec6028dd1623e4251b1b95dad522178517
Samuel Tennant <samueltennant@hotmail.com>
2023-04-07 21:10:44 +0100

    added api subroute

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,15 @@
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
 
 mod routes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(routes::index))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(routes::index)
+            .service(web::scope("/api").service(routes::health))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
--- server/src/routes.rs
+++ server/src/routes.rs
@@ -1,6 +1,12 @@
 use actix_web::{get, HttpResponse, Responder};
+use serde_json::json;
 
 #[get("/")]
 pub async fn index() -> impl Responder {
     HttpResponse::Ok().body("<h1>Hello world!</h1>")
+}
+
+#[get("/health")]
+pub async fn health() -> impl Responder {
+    HttpResponse::Ok().json(json!({"alive": "true"}))
 }
