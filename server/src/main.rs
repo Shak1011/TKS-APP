a1580b91076467d186503aedb32263b7a8afb583
Samuel Tennant <samueltennant@hotmail.com>
2023-04-07 20:57:53 +0100

    reorganised imports

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,10 @@
+use actix_web::{App, HttpServer};
+
 mod routes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    actix_web::HttpServer::new(|| actix_web::App::new().service(routes::index))
+    HttpServer::new(|| App::new().service(routes::index))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
